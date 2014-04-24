11327dd7c633cd55fb5d88c57fc08e1b88b7d803
Lynn Riddlespur <webdevdea@gmail.com>
2014-04-23 20:22:20 -0400

    Create racedaymap.php
    A sample map for the mock race registration

---
+++ racedaymap.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="Angels 5K fun run" />
+
+<meta name="keywords" content="run race 5 k angels baseball" />
+
+<meta name="author" content="DANDEWEBWONDERS" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>Angels 5K &amp; 1 Mile Family Fun Run/Walk </title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+<div id="content">
+
+<h1>RACE DAY MAP</h1>
+<p>
+
+
+<img src="5k_course_map.jpg" alt="RACEMAP" width="500" height="700" ALIGN="CENTER">
+</p>
+
+
+</div> <!-- end #content -->
+
+
+
+<?php include('footer.php'); ?>
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
