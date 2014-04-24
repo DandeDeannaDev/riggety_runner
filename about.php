7e8f5b5a5cfffc2456a8ca2f389974b0c87ca6b7
Lynn Riddlespur <webdevdea@gmail.com>
2014-04-23 20:27:54 -0400

    Create about.php
    This file tells about the charity the fake race is dedicated to

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="Angels 5K fun run" />
+
+<meta name="keywords" content="run race 5 k angels baseball" />
+
+<meta name="author" content="DANDEWEBWONDERS" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>Angels 5K &amp; 1 Mile Family Fun Run/Walk </title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+<div id="content">
+
+<h1>Charity</h1>
+
+<p>
+Proceeds from the event will benefit the Angels Baseball Foundation and its mission to create and improve  education, healthcare, arts & sciences, and community related youth programs throughout the region, in addition to providing children the opportunity</br> to experience the great game of baseball and its countless positive attributes.
+
+
+The Angels 5K and 1 Mile Family Fun Run/Walk presented by St. Joseph Health benefits Angels Baseball Foundation. The Angels work with various groups that promote the health of the community through awareness, treatment and research programs. The club teams with many healthcare organizations with a common goal of improving the lives of others.
+
+The Foundation also takes great pride in supporting nation-wide youth programs such as the Boys & Girls Clubs as well as providing financial assistance to youth baseball clinics, camps and recreational programs that promote the game of baseball and softball. Together with the Angels Baseball Foundation, these local and national organizations help shape the lives of thousands and, in many cases, help nurture the hopes and dreams of the countless youths involved.</P>
+<h1>Contact Information</h1>
+<p>
+Email: info@spectrumsports.net</br>
+Spectrum Sports Management - 909-399-3553
+</p>
+
+
+
+</div> <!-- end #content -->
+
+
+
+<?php include('footer.php'); ?>
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
