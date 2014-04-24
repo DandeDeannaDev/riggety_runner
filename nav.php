e9f81377650495622aebadb96bce222ec05e7083
Lynn Riddlespur <webdevdea@gmail.com>
2014-04-23 20:31:02 -0400

    Create nav.php
    create navigation menu

---
+++ nav.php
@@ -0,0 +1,8 @@
+<div id="nav">
+
+	<a href="Assignment12.php">Home</a>
+	<a href="about.php">CHARITY</a>
+	<a href="racedaymap.php">RACE DAY MAP</a>
+	<a href="insert.php">REGISTER</a>
+	
+</div> <!-- end #nav -->
