37d7546c7d8e5ac9861b669754bf3602ae6e9ac8
Lynn Riddlespur <webdevdea@gmail.com>
2014-04-23 20:24:13 -0400

    Create index.php
    Main page for the Mock race site

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="Race Registration Site" />
+
+<meta name="keywords" content="Race Register " />
+
+<meta name="author" content="DANDEWEBWONDERS" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>Angels 5K &amp; 1 Mile Family Fun Run/Walk </title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<?php include('header.php'); ?>
+
+<?php include('nav.php'); ?>
+
+<div id="content">
+
+
+
+<h1> Angels 5K & 1 Mile Family Fun Run/Walk Race</h1>
+
+
+
+
+    <p>Saturday, March 29, 2014<br/>
+    5K Run/Walk<br/>
+    1 Mile Family Fun Run/Walk</br></p>
+
+
+<h1> Race Course</h1>
+
+
+<p>  All races will start and finish at Angel Stadium of Anaheim</p>
+
+
+
+<h1>Entry Fees</h1>
+<p>    $40 for 5K</br>
+    $30 for Family Fun Run</p>
+
+
+
+
+
+</div> <!-- end #content -->
+
+
+
+<?php include('footer.php'); ?>
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
